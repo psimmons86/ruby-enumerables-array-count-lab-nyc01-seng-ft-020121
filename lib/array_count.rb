a4d38302d9828c4248e9008118c1ea14aa90525c
patrick simmons <hadroncollides@gmail.com>
2021-01-26 18:49:41 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,15 @@
+
+
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
-end
+
+array.count("foo") 
+
+
+
+  end
 
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
+  array.count("")
 end
